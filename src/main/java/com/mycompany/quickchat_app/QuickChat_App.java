02f221be523eedf89d12c94bf88c494bf3e352c4
AshlynWhittaker <ST10539059@rcconnect.edu.za>
2026-05-25 18:19:14 +0200

    First Commit!

---
+++ src/main/java/com/mycompany/quickchat_app/QuickChat_App.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.quickchat_app;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ashlyn Whittaker
+ */
+
+public class QuickChat_App {
+ 
+    public static void main(String[] args) {
+        //create an object for scanner class
+        Scanner input = new Scanner(System.in);
+        
+        //create message object
+        Message message = new Message();
+        
+        message.runQuickChat(input);
+
+
+
+        Login login =new Login();
+
+        //Declaring variables
+        String firstName, secondName, userName, password, cellPhoneNumber;
+
+        //ask user to input
+        System.out.println("Please enter your firstName:");
+        firstName = input.nextLine();
+        System.out.println("Please enter your secondName:");
+        secondName = input.nextLine();
+        System.out.println("Please enter your userName:");
+        userName = input.nextLine();
+        //validate
+        if (login.checkuserName(userName)) {
+            System.out.println("username successfully captured:");
+        } else {
+            System.out.println("incorrectly formatted:");
+        }
+
+        System.out.println("Please enter your password:");
+        password = input.nextLine();
+        //validate
+         if (login.checkPasswordComplexity(password)) {
+            System.out.println("Password sucessfully captured");
+             
+         }else{
+             System.out.println("Password not correctly formatted ,please ensure that password contains atleast eight charactes long , a number and special character");
+         }
+                
+        System.out.println("Please enter your cellPhoneNumber:");
+        cellPhoneNumber = input.nextLine();
+        //validate   
+        if (login.checkCellphoneNumber(cellPhoneNumber)) {
+            System.out.println("Cellphone number successfully added.");
+        } else {
+            System.out.println("Cellphone number incorrect.");
+        }
+
+        //display the registration class 
+        System.out.println(login.registerUser(firstName, secondName, userName, cellPhoneNumber));
+
+        //declare temporary variables to allow user to login 
+        String loginUsername, loginPassword;
+
+        System.out.println("***********LOGIN***********");
+        System.out.println("Please enter your login username:");
+        loginUsername = input.nextLine();
+        System.out.println("Please enter your login password:");
+        loginPassword = input.nextLine();
+       System.out.println(login.returnLoginStatus(firstName, secondName, userName, password, loginUsername, loginPassword));
+            
+    }
+} 
+
+   
+
+
+    
+         
+    
+       
+     
+
+    
+    
+
+   
+    
